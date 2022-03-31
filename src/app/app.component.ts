0803e141438fe07d6acc5047b90ff70809be17b9
Martin Pastorino <martinpastorino013@gmail.com>
2022-03-31 17:52:40 -0300

    ngFor, Bootstrap, Popper, Jquery, formulario de empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empelados';
+  empleados: Empleado [] = [
+
+    new Empleado("Martin","Pastorino","Presidente", 7500),
+    new Empleado("Ana","Planeta","Secretaria", 3000),
+    new Empleado("Jimena","Hicieron","Emperatriz", 10000),
+    new Empleado("David","River","Rey", 5000),
+  ];
+  
+  cuadroNombre:string = "";
+  cuadroApellido:string = "";
+  cuadroCargo:string = "";
+  cuadroSalario:number = 0;
+
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido,this.cuadroCargo, this.cuadroSalario);
+
+    this.empleados.push(miEmpleado);
+  }
 }
