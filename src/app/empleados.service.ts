74c12621a07423350af94b4c192472cf96e8d09a
Martin Pastorino <martinpastorino013@gmail.com>
2022-04-01 19:42:58 -0300

    service dentro de otro service

--- src/app/empleados.service.ts
+++ src/app/empleados.service.ts
@@ -1,10 +1,18 @@
+import { Injectable } from "@angular/core";
 import { Empleado } from "./empleado.model";
+import { ServicioEmpleadosService } from "./servicio-empleados.service";
 
+@Injectable()
 export class EmpleadosService{
 
     empleados: Empleado [] = [];
 
+    constructor(private ventanaEmergente: ServicioEmpleadosService){
+
+    }
+
     agregarEmpleadoServicio(empleado: Empleado){
+        this.ventanaEmergente.muestraMensaje("Persona se que se va a agregar: " + empleado.nombre)
         this.empleados.push(empleado);
     }
 }
